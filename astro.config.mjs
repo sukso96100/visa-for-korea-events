482daa0c5ad5c37c352209fdb968264e6d222278
Youngbin Han <sukso96100@gmail.com>
2024-07-09 06:39:22 +0000

    웹사이트 정보 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,16 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Visa for Korea Events',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: '가이드',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '시작하기', slug: 'guides/get-started' },
 					],
 				},
 				{
